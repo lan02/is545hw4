db0e1e2d818a46ec9e504641cb71aa21f1bf7cc6
Lan Luong <lan.luong@student.csulb.edu>
2019-03-12 21:59:16 -0700

    update version

--- script.js
+++ script.js
@@ -1,50 +1,43 @@
 function status(response) {
-    if (response.status >= 200) {
-      return Promise.resolve(response)
-    } else {
-      return Promise.reject(new Error(response.statusText))
-    }
+  if (response.status === 200) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(new Error(response.statusText))
   }
+}
+
+function json(response) {
+  return response.json()
+}
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(status)
+  .then(json)
+  .then(function(data) {
+    console.log('Request succeeded', data);
+    let arrUsername = data.map(a => a.username);
+    arrUsername.sort((a, b) => a.length - b.length);
+    document.getElementById("fetchdata").innerHTML = arrUsername.join("<br>");
+  })
+  .catch(function(error) {
+    console.log('Request failed', error);
+  });
   
-  function json(response) {
-    return response.json()
-  }
-  
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(status)
-    .then(json)
-    .then(function(data) {
-      console.log('Request succeeded', data);
-      let arrUsername = data.map(a => a.username);
-      arrUsername.sort((a, b) => a.length - b.length);
-      let username = "";
-      for(i =0; i < arrUsername.length; i++) {
-        username += arrUsername[i] + "<br>";
-    }
-    document.getElementById("fetchdata").innerHTML = username; 
-    }).catch(function(error) {
-      console.log('Request failed', error);
-    });
-    
-  const req = new XMLHttpRequest(); 
-  req.open('GET','https://jsonplaceholder.typicode.com/users'); 
-  req.onload = function () { 
-      const arrUser = JSON.parse(req.response);
-      let arrEmail = arrUser.map(a => a.email);
-      arrEmail.sort();
-      let emails = "";
-      if (req.status == 200) { 
-          console.log ('Request succeeded', arrUser);
-          for(i =0; i < arrEmail.length; i++) {
-              emails += arrEmail[i] + "<br>";
-          }
-          document.getElementById("output").innerHTML = emails; 
-      } else { 
-          console.log('ERROR', req.statusText); 
-      } 
-  };
-  req.onerror = function () { 
-      console.log('Network Error'); 
-  }; 
-  req.send(); // Add request to task queue
+const req = new XMLHttpRequest(); 
+req.open('GET','https://jsonplaceholder.typicode.com/users'); 
+req.onload = function () { 
+    const arrUser = JSON.parse(req.response);
+    let arrEmail = arrUser.map(a => a.email);
+    arrEmail.sort();
+    if (req.status === 200) { 
+      console.log ('Request succeeded', req.responseText);
+      document.getElementById("output").innerHTML = arrEmail.join("<br>");
+    } else { 
+      console.log('ERROR', req.statusText); 
+    } 
+};
+req.onerror = function () { 
+    console.log('Network Error'); 
+}; 
+req.send(); // Add request to task queue
 
